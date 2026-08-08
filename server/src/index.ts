21fc5e9c22b4715c3e0a45e29b75f4fffd59ce62
rqeeb <raqeebauto@gmail.com>
2026-08-08 12:25:26 +0530

    finally somehow setupped prisma 7 - first time setting it up rahh

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,28 @@
 import express from "express";
 import cors from "cors";
+import { prisma } from "./lib/db.ts";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-    origin:"http://localhost:3000",
-    credentials:true
-}))
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+);
 
 app.listen(2020);
 
-app.get("/health",(req,res)=>{
-res.json({
-    message:"server is running.... 67676"
-})
-})
+app.get("/health", async (req, res) => {
+//   const createUser = await prisma.user.create({
+//     data: {
+//       email: "shsh@gmail.com",
+//       passwordHash: "1234",
+//     },
+//   });
+
+  res.json({
+    message: "server is running.... 67676",
+  });
+});
