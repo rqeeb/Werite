feba23c3d0ece3211a504edd6dfa7ac2e7f9e185
rqeeb <raqeebauto@gmail.com>
2026-08-09 12:20:47 +0530

    type decalration

---
+++ server/src/types/express.d.ts
@@ -0,0 +1,14 @@
+import "express";
+
+declare global {
+    namespace Express {
+        interface Request {
+            userId?:string
+        }
+        interface Response {
+            
+        }
+    }
+}
+
+export{}
