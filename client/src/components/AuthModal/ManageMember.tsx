10d1f7795e564b18454f6c815f41728ac013c6d2
rqeeb <raqeebauto@gmail.com>
2026-08-28 20:09:51 +0530

    more styles and mnfgrfb

--- client/src/components/AuthModal/ManageMember.tsx
+++ client/src/components/AuthModal/ManageMember.tsx
@@ -1,3 +1,5 @@
+import { Trash } from "lucide-react";
+
 type ManageMemberProps = {
   username: string;
   email: string;
@@ -5,20 +7,25 @@ type ManageMemberProps = {
 };
 
 function ManageMember({ username, email, role }: ManageMemberProps) {
-  const initial = (username || email).trim().charAt(0).toUpperCase();
-
   return (
     <div className="memberRow">
-      <div className="memberAvatar">{initial}</div>
-
       <div className="memberInfo">
         <p className="memberName">{username}</p>
         <p className="memberEmail">{email}</p>
       </div>
 
       <span className={`memberRole ${role.toLowerCase()}`}>
-        {role === "EDITOR" ? "Can edit" : "View only"}
+        {role === "EDITOR" ? "Editor" : "Viewer"}
       </span>
+
+      <button
+        type="button"
+        // className={`deleteDocumentButton user`}
+        // disabled={isDeleteLoading}
+        aria-label="Remove User"
+      >
+        <Trash size={15} />
+      </button>
     </div>
   );
 }
