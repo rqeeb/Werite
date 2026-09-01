2e1e7f6ab4d6b7cfbf0e4f982cdbba1f0c238fe9
rqeeb <raqeebauto@gmail.com>
2026-09-01 16:05:13 +0530

    lax

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -97,7 +97,7 @@ router.post("/login", async (req, res) => {
       .cookie("token", token, {
         httpOnly: true,
         secure: isProduction,
-        sameSite: isProduction? "none" : "lax",
+        sameSite: "lax",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       })
       .status(200)
